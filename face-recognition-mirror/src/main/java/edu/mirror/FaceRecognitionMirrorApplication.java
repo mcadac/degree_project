4d207d454e3a46ff14df2714d2b54c2ad4e1d67e
McadaC <dcespitiam@gmail.com>
2018-04-21 09:51:09 -0500

    Creates new config

---
+++ face-recognition-mirror/src/main/java/edu/mirror/FaceRecognitionMirrorApplication.java
@@ -0,0 +1,12 @@
+package edu.mirror.facerecognitionmirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceRecognitionMirrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceRecognitionMirrorApplication.class, args);
+	}
+}
